9b7c4379ec99eba76a007b3f0a2c0c4234903180
yuzhiqiang <zhiqiang.yu.xeon@gmail.com>
2026-08-01 15:00:46 +0800

    clean up temp scripts

--- patch3.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let cap = JSON.parse(fs.readFileSync('src-tauri/capabilities/default.json', 'utf-8'));
-if (!cap.permissions.includes("dialog:default")) {
-  cap.permissions.push("dialog:default");
-}
-fs.writeFileSync('src-tauri/capabilities/default.json', JSON.stringify(cap, null, 2));
